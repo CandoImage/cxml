c3a53362e6d71a91533ac1e31e1e446ab9fc706d
Peter Philipp <peter.philipp@cando-image.com>
2021-11-03 15:52:42 +0100

    Added itemID getter / setter to SelectedItem

--- src/CXml/Models/Messages/SelectedItem.php
+++ src/CXml/Models/Messages/SelectedItem.php
@@ -17,4 +17,25 @@ class SelectedItem implements RequestInterface
         $this->itemID = new ItemId();
         $this->itemID->parse(current($requestNode->xpath('ItemID')));
     }
+
+    /**
+     * @return \CXml\Models\Messages\ItemId
+     */
+    public function getItemID(): ItemId
+    {
+        return $this->itemID;
+    }
+
+    /**
+     * @param \CXml\Models\Messages\ItemId $itemID
+     *
+     * @return SelectedItem
+     */
+    public function setItemID(ItemId $itemID): self
+    {
+        $this->itemID = $itemID;
+        return $this;
+    }
+
+
 }
